e97683e67416d4f3b818767437a5ffda4ac3fd9b
Alejandro Guerrero <alejo.jesus.magne@gmail.com>
2022-05-30 17:51:01 -0700

    Policy laravel

--- app/Http/Controllers/Admin/PostController.php
+++ app/Http/Controllers/Admin/PostController.php
@@ -83,6 +83,7 @@ class PostController extends Controller
      */
     public function edit(Post $post)
     {
+      $this->authorize('author', $post);
       $categories = Category::pluck('name', 'id');
       $tags       = Tag::all();
       return view('admin.posts.edit', compact('post', 'categories', 'tags'));
@@ -97,6 +98,7 @@ class PostController extends Controller
      */
     public function update(PostRequest $request, Post $post)
     {
+      $this->authorize('author', $post);
       $post->update($request->all());
 
       if( $request->file('file') ) {
@@ -134,6 +136,7 @@ class PostController extends Controller
      */
     public function destroy(Post $post)
     {
+      $this->authorize('author', $post);
       $post->delete();
       return redirect()->route('admin.posts.index')->with('info', 'The post has been successfully deleted');
     }
