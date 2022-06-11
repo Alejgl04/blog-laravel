b9bbba6efe577d13a190fdbc251728f0a0e66bfc
Alejandro Guerrero <alejo.jesus.magne@gmail.com>
2022-06-10 19:09:48 -0700

    if else category tag view

--- resources/views/posts/category.blade.php
+++ resources/views/posts/category.blade.php
@@ -2,13 +2,20 @@
 
   <div class="max-w-5xl mx-auto px-2 sm:px-6 lg:px-8 py-8">
     <h1 class="uppercase text-center text-3xl font-bold">Categoría: {{ $category->name }}</h1>
-    @foreach ($posts as $post)
-      <x-card-post :post="$post"/>
-    @endforeach
+    @if ( $posts->count() )
 
-    <div class="mt-4">
-      {{ $posts->links() }}
-    </div>
+      @foreach ($posts as $post)
+        <x-card-post :post="$post"/>
+      @endforeach
+
+      <div class="mt-4">
+        {{ $posts->links() }}
+      </div>
+    @else
+      <div class="text-center">
+        We are sorry, but at this moment there aren't posts to show with this category
+      </div>
+    @endif
   </div>
 
 </x-app-layout>
--- resources/views/posts/tag.blade.php
+++ resources/views/posts/tag.blade.php
@@ -2,10 +2,20 @@
 
   <div class="max-w-5xl mx-auto px-2 sm:px-6 lg:px-8 py-8">
     <h1 class="uppercase text-center text-3xl font-bold">Etiqueta: {{ $tag->name }}</h1>
+    @if ( $posts->count() )
+
     @foreach ($posts as $post)
       <x-card-post :post="$post"/>
     @endforeach
 
+    <div class="mt-4">
+      {{ $posts->links() }}
+    </div>
+  @else
+    <div class="text-center">
+      We are sorry, but at this moment there aren't posts to show with this tag
+    </div>
+  @endif
     <div class="mt-4">
       {{ $posts->links() }}
     </div>
