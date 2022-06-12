c5a4b46aa6363348e136f6babb0d4b2aad41c624
Alejandro Guerrero <alejo.jesus.magne@gmail.com>
2022-06-12 16:55:14 -0700

    testing email

--- config/mail.php
+++ config/mail.php
@@ -36,11 +36,11 @@ return [
     'mailers' => [
         'smtp' => [
             'transport' => 'smtp',
-            'host' => env('MAIL_HOST', 'smtp.mailgun.org'),
-            'port' => env('MAIL_PORT', 587),
+            'host' => env('MAIL_HOST', 'mail.salvadorhairdressing.com'),
+            'port' => env('MAIL_PORT', 465),
             'encryption' => env('MAIL_ENCRYPTION', 'tls'),
-            'username' => env('MAIL_USERNAME'),
-            'password' => env('MAIL_PASSWORD'),
+            'username' => env('MAIL_USERNAME','atc@salvadorhairdressing.com'),
+            'password' => env('MAIL_PASSWORD','atencion.14'),
             'timeout' => null,
         ],
 
